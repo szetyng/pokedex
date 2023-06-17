6c7bb9fc00d585b9e8df2c076f824e584c1fe5f4
Sze Tyng <lee.szetyng@gmail.com>
2023-06-17 20:05:22 +0800

    add primevue

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import "./index.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
 
-createApp(App).mount("#app");
+import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primevue/resources/primevue.min.css";
+// import "primeicons/primeicons.css";
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount("#app");
