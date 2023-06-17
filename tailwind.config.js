08f95c561b13ac7a0abb5dd977614becf048c289
Sze Tyng <lee.szetyng@gmail.com>
2023-06-17 20:15:47 +0800

    change colors in tailwind and add font in CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FBBF24",
+        hover: "#D97706",
+        background: "#FDE68A",
+        "card-background": "#FFFFFF",
+        text: "#0F172A",
+        "secondary-text": "#94A3B8",
+        "empty-field": "#F1F5F9",
+        "filled-heart": "#EF4444",
+      },
+    },
   },
   plugins: [],
 };
