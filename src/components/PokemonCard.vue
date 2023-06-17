7bc80ea70138d1ab8f329e6eb1dbff2fa34e75a9
Sze Tyng <lee.szetyng@gmail.com>
2023-06-17 21:56:16 +0800

    add dialog box on click

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -18,11 +18,43 @@ const fetchPokemonDetails = async (pokemonUrl) => {
 };
 
 fetchPokemonDetails(props.url);
+
+const dialog = ref(false);
+const openDialog = () => {
+  dialog.value = true;
+};
+
+const closeDialog = () => {
+  dialog.value = false;
+};
 </script>
 
 <template>
-  <div class="bg-card-background p-4 h-full rounded-lg">
+  <div
+    class="bg-card-background p-4 h-full rounded-lg hover:cursor-pointer"
+    @click="openDialog"
+  >
     <img class="w-full" :src="imageUrl" :alt="`Picture of ${name}`" />
     <p class="text-center">{{ name }}</p>
+  </div>
+
+  <!-- Dialog -->
+  <div v-if="dialog" class="fixed inset-0 flex items-center justify-center">
+    <!-- Dialog grey thing -->
+    <div class="bg-gray-800 bg-opacity-50 absolute inset-0"></div>
+
+    <!-- Dialog card -->
+    <div class="bg-card-background p-8 rounded-lg z-10 w-9/12">
+      <!-- Dialog content -->
+      <h2 class="text-xl mb-4">{{ name }}</h2>
+      <p>Pika pika.</p>
+
+      <button
+        class="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+        @click="closeDialog"
+      >
+        Close
+      </button>
+    </div>
   </div>
 </template>
