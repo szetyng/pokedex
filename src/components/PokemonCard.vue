5a7587652cc4b4f4e07294fefb3345893383d195
Sze Tyng <lee.szetyng@gmail.com>
2023-06-17 22:10:38 +0800

    switch to bigger artwork and display names and IDs properly

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -10,15 +10,17 @@ const props = defineProps({
 const pokemonId = ref(0);
 const imageUrl = ref(null);
 
+// On load, get additional pokemon details for card
 const fetchPokemonDetails = async (pokemonUrl) => {
   const { id, sprites } = await getPokemonByUrl(pokemonUrl);
 
   pokemonId.value = id;
-  imageUrl.value = sprites.front_default;
+  imageUrl.value = sprites.other["official-artwork"].front_default;
 };
 
 fetchPokemonDetails(props.url);
 
+// Handle dialog popup
 const dialog = ref(false);
 const openDialog = () => {
   dialog.value = true;
@@ -26,6 +28,15 @@ const openDialog = () => {
 
 const closeDialog = () => {
   dialog.value = false;
+};
+
+// Additional functions to help display card
+const paddifyId = (number) => {
+  return String(number).padStart(4, "0");
+};
+
+const capitaliseFirst = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 };
 </script>
 
@@ -35,7 +46,7 @@ const closeDialog = () => {
     @click="openDialog"
   >
     <img class="w-full" :src="imageUrl" :alt="`Picture of ${name}`" />
-    <p class="text-center">{{ name }}</p>
+    <p class="text-center">{{ capitaliseFirst(name) }}</p>
   </div>
 
   <!-- Dialog -->
@@ -44,17 +55,21 @@ const closeDialog = () => {
     <div class="bg-gray-800 bg-opacity-50 absolute inset-0"></div>
 
     <!-- Dialog card -->
-    <div class="bg-card-background p-8 rounded-lg z-10 w-9/12">
+    <div class="bg-card-background p-8 rounded-lg z-10 w-9/12 grid grid-cols-3">
       <!-- Dialog content -->
-      <h2 class="text-xl mb-4">{{ name }}</h2>
-      <p>Pika pika.</p>
+      <div class="col-span-1">
+        <img class="w-full" :src="imageUrl" :alt="`Picture of ${name}`" />
+        <h2 class="text-xl">{{ name }}</h2>
+        <p class="text-secondary-text text-xl">{{ paddifyId(pokemonId) }}</p>
 
-      <button
-        class="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
-        @click="closeDialog"
-      >
-        Close
-      </button>
+        <button
+          class="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+          @click="closeDialog"
+        >
+          Close
+        </button>
+      </div>
+      <div class="col-span-2">hlleo</div>
     </div>
   </div>
 </template>
