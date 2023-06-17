c629f96ad509936adf3df817bf8f76ed0160e35e
Sze Tyng <lee.szetyng@gmail.com>
2023-06-17 20:45:45 +0800

    add placeholder pokemon card and css to display the homepage with columns

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup></script>
+<script setup>
+import PokemonCard from "./components/PokemonCard.vue";
+</script>
 
 <template>
   <header>
-    <div>Hello</div>
+    <div>
+      <span class="text-xl">Pokédex</span>
+    </div>
   </header>
 
-  <main>Main</main>
+  <main>
+    <span>Search for a Pokémon by name or id number</span>
+
+    <div class="grid grid-cols-12 gap-4">
+      <div class="grid grid-cols-6 gap-4 col-span-9">
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+        <PokemonCard />
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+import Card from "primevue/card";
+
+const pokemon = ref({
+  name: "Pikachu",
+  id: 1,
+});
+</script>
+
+<template>
+  <Card>
+    <template #title>{{ pokemon.name }}</template>
+  </Card>
+</template>
